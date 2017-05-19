c320d8375417c104340c66433cce52604fd383ad
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-05-19 19:49:30 +0200

    16

--- src/ha/MySortedList.java
+++ src/ha/MySortedList.java
@@ -13,6 +13,41 @@ package ha;
 public class MySortedList<T> extends MyList implements Comparable<T>{
 
     @Override
+    public void insert(Object e) {
+        this.sortedInsert(e);
+    }   
+
+    public void sortedInsert(Comparable e) {
+        if (this.length() > 0) {
+            MyListElement current = firstElement;
+            if (current.getMyElement() != null) {
+                while (current.getMyElement() != null && current.getnextMyElement() != null) {
+                    if (current.getnextMyElement().getMyElement() == null) {
+                        if (current.getMyListElement().compareTo(e) > 0) {
+                            this.insert(e);
+                        } else {
+                            this.append(e);
+                        }
+                        break;
+                    }
+                    if (current.getMyListElement().compareTo(e) <= 0 && current.getnextMyElement().getMyListElement().compareTo(e) >= 0) {
+                        MyListElement element = (MyListElement) e;
+                        element.setnextMyElement(current.getnextMyElement());
+                        element.setprevMyElement(current);
+                        current.setnextMyElement(element);
+                        element.getnextMyElement().setprevMyElement(element);
+                        break;
+                    }
+                    
+                    current = current.getnextMyElement();
+                }    
+            } 
+        } else {
+            throw new IndexOutOfBoundsException("Liste hat noch keine Elemente!");
+        }
+    }
+    
+    @Override
     public void append(Object e) {
         throw new UnsupportedOperationException("Append ist nicht zugelassen für Sortierte Listen.");
     }
