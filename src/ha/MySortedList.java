f78fc773347491cde34704614ae3819958e34578
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-05-19 19:45:24 +0200

    15

---
+++ src/ha/MySortedList.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ha;
+
+/**
+ *
+ * @author Markus
+ */
+public class MySortedList<T> extends MyList implements  Comparable<T>{
+
+    
+    @Override
+    public int compareTo(T t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
