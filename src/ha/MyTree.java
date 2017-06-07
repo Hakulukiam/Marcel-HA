4871bc2e93570fa2b64c32e9842ffbb7220f4805
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-06-07 12:20:02 +0200

    50

--- src/ha/MyTree.java
+++ src/ha/MyTree.java
@@ -63,13 +63,11 @@ public class MyTree<K extends Comparable<K>, T> implements de.tu_bs.ips.Tree, Co
 
     @Override
     public int size() {
-        int counter;
         if(!this.isEmpty()){
-           counter = this.getLeftchild().size()+this.getRightchild().size();
+            return 1 + this.getLeftchild().size() + this.getRightchild().size();
         }else{
             return 0;
         }
-        return counter;
     }
 
     @Override
